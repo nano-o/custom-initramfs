373d1c392c5e62681f489dd0eabb4a4f4c82ab54
Giuliano Losa <giuliano@losa.fr>
2015-12-03 11:49:47 -0500

    Created a basic script to create symlinks for all the busybox utilities.

---
+++ create_busybox_links.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+verbose="n"
+
+# $1 is the root of the image
+image_root=$1
+
+# location of busybox on the fs of the image:
+busybox="/bin/busybox"
+
+command -v busybox >/dev/null 2>&1 || { echo >&2 "error: busybox is not installed.  Aborting."; exit 1; }
+
+busybox_target=${image_root}$busybox
+
+if [ ! -f $busybox_target ]; then
+    echo "error: busybox has not been copied to the image. Aborting."
+    exit 1
+fi
+
+list=`busybox --list-all`
+
+for f in $list; do
+    target=${image_root}$f
+    dirname=$(dirname "$target")
+    if [ ! -d "$dirname" ]; then
+        [ "${verbose}" = "y" ] && echo "creating directory $dirname"
+        mkdir -p "$dirname"
+    fi
+    [ "${verbose}" = "y" ] && echo "creating symlink $target"
+    ln -s $busybox $target
+done
