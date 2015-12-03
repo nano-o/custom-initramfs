d87324854ea558e5d4e32289fb3ba05ae0ee2333
Giuliano Losa <giuliano@losa.fr>
2015-12-03 12:14:21 -0500

    Created a script to package a dir contain the files of the image as a cpio archive.

---
+++ package_image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+usage="Usage: package_image.sh image_root cpio_filename_absolute"
+image_root=$1
+target=$2
+
+
+if [ ! -n $image_root ] || [ ! -d $image_root ]; then
+    echo >&2 "error: image root not given or not found. Aborting"
+    echo >&2 $usage
+    exit 1
+fi
+
+if [ ! -n $target ]; then
+    echo >&2 "error: filename of the cpio archive to be created not given. Aborting"
+    echo >&2 $usage
+    exit 1
+fi
+
+if [[ ! $target = /* ]]; then
+    echo >&2 "error: filename of the cpio archive to be created is not an absolute path. Aborting"
+    exit 1
+fi
+
+(cd $image_root && find . -print0 | cpio --null -ov --format=newc | gzip -9 > $target)
